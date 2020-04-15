23c4082f2ba1c1dbaa4e88fbd0a824e1a66a1731
Artem <artemklishch@gmail.com>
2020-04-15 09:26:19 +0300

    fgh

--- lesson24/task2/index.js
+++ lesson24/task2/index.js
@@ -11,6 +11,7 @@ const getChangedArrayOfUsersData = (personArray, conversationArray) => {
   return newCommonArray;
 }
 
+
 const person = [
   { id: "32323232", name: "Jhohn" },
   { id: "12321321312", "name": "Peter" }
@@ -20,7 +21,8 @@ const conversation = [
   { id: "323232", person: "32323232", speech: "hello" },
   { id: "243232", person: "12321321312", speech: "hi, how's going" },
   { id: "121212", person: "32323232", speech: "I'm fine" },
-  { id: "21322", person: "32323232", speech: "How about yourself?" }
+  { id: "21322", person: "32323232", speech: "How about yourself?" },
+  { id: "21322", person: "4444", speech: "How about yourself?" }
 ];
 
 console.log(getChangedArrayOfUsersData(person, conversation));
